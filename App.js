2b01d4a34b960fdd07cadd9cc3f6c3b6b5645f28
Ridwan Kadri <kadriridwan0@gmail.com>
2021-05-27 02:30:26 +0000

    built the login and register screen

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: '#2c6bed'},
+  headerTitleStyle: {color: 'white'},
+  headerTintColor: 'white',
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={LoginScreen}/>
+        <Stack.Screen name='Register' component={RegisterScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQFdl3TEe6RYyRJRoJQqOh-IznA5sB1hg",
+    authDomain: "signal-clone-c25df.firebaseapp.com",
+    projectId: "signal-clone-c25df",
+    storageBucket: "signal-clone-c25df.appspot.com",
+    messagingSenderId: "802815393851",
+    appId: "1:802815393851:web:50c40d2a17aba296f41c1e"
+  };
